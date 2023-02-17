f19dbee7e226484db60f3fafec2363f24fad9dd8
camin-mccluskey <c.mccluskey_7@hotmail.co.uk>
2023-02-17 14:43:25 +0000

    Clean up commands

--- src/roam---edit-daily-note.tsx
+++ src/roam---edit-daily-note.tsx
@@ -1,33 +1,84 @@
-import { Form, getPreferenceValues, useNavigation } from "@raycast/api";
+import { Action, ActionPanel, Form, getPreferenceValues, useNavigation } from "@raycast/api";
 import { useEffect, useState } from "react";
 import RoamPrivateApi from './RoamPrivateApi';
 
-interface Preferences {
+type Preferences = {
   email: string;
   password: string;
   graph: string;
 }
 
-let roamApi: RoamPrivateApi;
+// const cache = new Cache();
 
 
 export default function Command() {
   const { email, password, graph } = getPreferenceValues<Preferences>(); 
   const { pop } = useNavigation();
+  const [roamApi, setRoamApi] = useState<RoamPrivateApi | null>(null);
+  const [title, setTitle] = useState('');
   const [isLoading, setIsLoading] = useState(false);
 
+  // possibly reintroduce caching
+  // const setCache = (roamApi: RoamPrivateApi) => {
+  //   cache.set('roam-api', JSON.stringify({cachedApi: instanceToPlain(roamApi), timestamp: new Date().getTime()}));
+  //   console.log('cached roam api');
+
+  // }
+
   useEffect(() => {
-    // think about caching
-    roamApi = new RoamPrivateApi(graph, email, password, {headless: true, folder: '', nodownload: false});    
+    // if (cache.has('roam-api')) {
+    //   console.log('using cached roam api')
+    //   const { cachedApi, timestamp } = JSON.parse(cache.get('roam-api') as string) as CachedRoamApi;
+    //   if (new Date().getTime() - timestamp > 1000 * 60) {
+    //     console.log('cached roam api expired');
+    //     cache.remove('roam-api');
+    //     setRoamApi(null);
+    //   } else {
+    //     console.log(cachedApi)
+    //     setRoamApi(plainToInstance(RoamPrivateApi, cachedApi));
+    //     return;
+    //   }
+    // }
+    console.log('starting login')
+    const newRoamApi = new RoamPrivateApi(
+      graph, email, password,
+      (roamApi: RoamPrivateApi) => console.log('login finished'),
+      {headless: true, folder: '', nodownload: false}
+    );
+    setRoamApi(newRoamApi);
   }, [email, password, graph]);
+
+  const onSubmit = () => {
+    if (!roamApi) return;
+    setIsLoading(true);
+    roamApi.createDailyNoteBlock(title).then((res) => {
+      console.log('success');
+      console.log(res)
+    }).catch((err) => {
+      console.log(err);
+    }).finally(() => {
+      setIsLoading(false);
+      pop();
+    });
+  };
   
   return (
-    <Form>
+    <Form
+      isLoading={isLoading}
+      navigationTitle='Create New Block'
+      actions={
+        <ActionPanel>
+          <Action.SubmitForm title="Create" onSubmit={onSubmit} />
+        </ActionPanel>
+      }
+    >
       <Form.TextField
-        // title="Block"
+        title="Block Title"
         id="name"
-        defaultValue={''}
+        placeholder="Block Title"
+        defaultValue={'Daily Note'}
+        onChange={setTitle}
       />
-  </Form>
-  );
+    </Form>
+  )
 }
